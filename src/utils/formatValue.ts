ce3c729cd52e2467e5f08378ccdd421e96ff9078
Beta Ortiz <emaildabetaortiz@gmail.com>
2020-10-31 22:06:19 -0300

    challenge done

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,10 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const newValue = new Intl.NumberFormat('pt', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+
+  return newValue;
+};
 
 export default formatValue;
